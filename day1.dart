cc748b143eff9dcf77b803b8adc5ec1763fe2bf1
bedirulu <bedirulu28@gmail.com>
2026-02-22 00:25:29 +0000

    flutterda material app komutu

---
+++ day1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Uygulamam());
+}
+
+class Uygulamam extends StatelessWidget {
+  const Uygulamam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Text('merhaba')));
+  }
+}
